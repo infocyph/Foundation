9871f74ffa69129b5b7816607c2b625db427de24
A. B. M. Mahmudul Hasan <abmmhasan@users.noreply.github.com>
2026-08-17 12:32:18 +0600

    foundation 2.0 architecture

---
+++ src/Runtime/ExecutionId.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Infocyph\Foundation\Runtime;
+
+final readonly class ExecutionId implements \Stringable
+{
+    public function __construct(public string $value)
+    {
+        if ($value === '') {
+            throw new \InvalidArgumentException('Execution ID cannot be empty.');
+        }
+    }
+
+    public static function generate(): self
+    {
+        return new self(bin2hex(random_bytes(16)));
+    }
+
+    public function __toString(): string
+    {
+        return $this->value;
+    }
+}
