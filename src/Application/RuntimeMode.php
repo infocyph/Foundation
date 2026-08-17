c0172428ffe08514a327b419996866e714a26248
A. B. M. Mahmudul Hasan <abmmhasan@users.noreply.github.com>
2026-08-17 12:31:24 +0600

    foundation 2.0 architecture

--- src/Application/RuntimeMode.php
+++ src/Application/RuntimeMode.php
@@ -6,7 +6,16 @@ namespace Infocyph\Foundation\Application;
 
 enum RuntimeMode: string
 {
-    case Console = 'console';
-
     case Web = 'web';
+
+    case Cli = 'cli';
+
+    case Worker = 'worker';
+
+    case Scheduler = 'scheduler';
+
+    public function isPersistent(): bool
+    {
+        return $this === self::Worker || $this === self::Scheduler;
+    }
 }
