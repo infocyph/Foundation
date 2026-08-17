c040def7e917d8b9c2d5a55b4d3ffaa6c8ead59c
A. B. M. Mahmudul Hasan <abmmhasan@users.noreply.github.com>
2026-08-17 12:42:35 +0600

    foundation 2.0 architecture

---
+++ src/Command/TerminalIO.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Infocyph\Foundation\Command;
+
+final readonly class TerminalIO implements CommandIO
+{
+    public function write(string $message): void
+    {
+        fwrite(STDOUT, $message);
+    }
+
+    public function writeln(string $message = ''): void
+    {
+        fwrite(STDOUT, $message . PHP_EOL);
+    }
+
+    public function error(string $message): void
+    {
+        fwrite(STDERR, $message . PHP_EOL);
+    }
+}
