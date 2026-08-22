7a3255b6bfaf01552fae5bd498500a96ce8f3214
A. B. M. Mahmudul Hasan <abmmhasan@users.noreply.github.com>
2026-08-22 21:46:05 +0600

    refactor: resolve repositories through DBLayer factory

--- src/Database/DatabaseRepository.php
+++ src/Database/DatabaseRepository.php
@@ -8,11 +8,12 @@ use Infocyph\DBLayer\DB;
 use Infocyph\DBLayer\Query\Repository;
 
 /**
- * Container-facing bridge to DBLayer's repository implementation.
+ * Container-facing bridge that applies Foundation connection selection to a
+ * native DBLayer repository.
  */
 abstract class DatabaseRepository extends Repository
 {
-    public function __construct(DatabaseManager $database)
+    public function __construct(DBLayerFactory $database)
     {
         $connection = $database->connection($this->connectionName());
 
