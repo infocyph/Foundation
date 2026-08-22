aa4652bc518dbbea2061a97a409f3f29528b5d55
A. B. M. Mahmudul Hasan <abmmhasan@users.noreply.github.com>
2026-08-22 10:25:11 +0600

    feat: add Foundation command exit codes

---
+++ src/Command/ExitCode.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Infocyph\Foundation\Command;
+
+final class ExitCode
+{
+    public const int SUCCESS = 0;
+
+    public const int FAILURE = 1;
+
+    public const int INVALID_USAGE = 2;
+
+    public const int CANNOT_EXECUTE = 126;
+
+    public const int COMMAND_NOT_FOUND = 127;
+
+    public const int INTERRUPTED = 130;
+
+    private function __construct() {}
+}
