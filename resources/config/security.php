2e46588d2ba9d411a0895dd391e540423fde858a
A. B. M. Mahmudul Hasan <abmmhasan@users.noreply.github.com>
2026-08-22 22:13:41 +0600

    refactor: use shared environment helpers in security config

--- resources/config/security.php
+++ resources/config/security.php
@@ -2,24 +2,6 @@
 
 declare(strict_types=1);
 
-$env = static function (string $key, mixed $default = null): mixed {
-    $value = getenv($key);
-
-    return $value === false ? $default : $value;
-};
-
-$envString = static function (string $key, string $default) use ($env): string {
-    $value = $env($key);
-
-    return is_string($value) && $value !== '' ? $value : $default;
-};
-
-$envInt = static function (string $key, int $default) use ($env): int {
-    $value = $env($key);
-
-    return is_numeric($value) ? (int) $value : $default;
-};
-
 return [
     /*
     |--------------------------------------------------------------------------
@@ -34,11 +16,11 @@ return [
     |
     */
     'password' => [
-        'algorithm' => $envString('SECURITY_PASSWORD_ALGORITHM', 'argon2id'),
-        'memory_cost' => $envInt('SECURITY_PASSWORD_MEMORY_COST', PASSWORD_ARGON2_DEFAULT_MEMORY_COST),
-        'time_cost' => $envInt('SECURITY_PASSWORD_TIME_COST', PASSWORD_ARGON2_DEFAULT_TIME_COST),
-        'threads' => $envInt('SECURITY_PASSWORD_THREADS', PASSWORD_ARGON2_DEFAULT_THREADS),
-        'cost' => $envInt('SECURITY_PASSWORD_BCRYPT_COST', 12),
+        'algorithm' => env_string('SECURITY_PASSWORD_ALGORITHM', 'argon2id'),
+        'memory_cost' => env_int('SECURITY_PASSWORD_MEMORY_COST', PASSWORD_ARGON2_DEFAULT_MEMORY_COST),
+        'time_cost' => env_int('SECURITY_PASSWORD_TIME_COST', PASSWORD_ARGON2_DEFAULT_TIME_COST),
+        'threads' => env_int('SECURITY_PASSWORD_THREADS', PASSWORD_ARGON2_DEFAULT_THREADS),
+        'cost' => env_int('SECURITY_PASSWORD_BCRYPT_COST', 12),
     ],
 
     /*
@@ -55,10 +37,10 @@ return [
     |
     */
     'jwt' => [
-        'algorithm' => $envString('SECURITY_JWT_ALGORITHM', 'HS256'),
-        'audience' => $env('SECURITY_JWT_AUDIENCE'),
-        'issuer' => $env('SECURITY_JWT_ISSUER'),
-        'maximum_lifetime_seconds' => $envInt('SECURITY_JWT_MAXIMUM_LIFETIME_SECONDS', 1209600),
-        'leeway_seconds' => $envInt('SECURITY_JWT_LEEWAY_SECONDS', 0),
+        'algorithm' => env_string('SECURITY_JWT_ALGORITHM', 'HS256'),
+        'audience' => env('SECURITY_JWT_AUDIENCE'),
+        'issuer' => env('SECURITY_JWT_ISSUER'),
+        'maximum_lifetime_seconds' => env_int('SECURITY_JWT_MAXIMUM_LIFETIME_SECONDS', 1_209_600),
+        'leeway_seconds' => env_int('SECURITY_JWT_LEEWAY_SECONDS', 0),
     ],
 ];
