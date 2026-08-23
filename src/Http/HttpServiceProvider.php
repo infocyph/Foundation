9d24beea049461cc2ca72ae0e50e6def26fb6284
A. B. M. Mahmudul Hasan <abmmhasan@users.noreply.github.com>
2026-08-23 13:15:06 +0600

    feat: inject maintenance state into web kernel

--- src/Http/HttpServiceProvider.php
+++ src/Http/HttpServiceProvider.php
@@ -10,6 +10,7 @@ use Infocyph\Foundation\Http\Response\AuthExceptionMapper;
 use Infocyph\Foundation\Http\Response\AuthResponseFactory;
 use Infocyph\Foundation\Http\Response\ExceptionRenderer;
 use Infocyph\Foundation\Logging\HttpExceptionLogger;
+use Infocyph\Foundation\Operations\MaintenanceManager;
 use Infocyph\Foundation\Routing\WebrickRouterFactory;
 use Infocyph\Foundation\Runtime\ExecutionScope;
 use Infocyph\InterMix\DI\Support\LifetimeEnum;
@@ -31,6 +32,12 @@ final class HttpServiceProvider extends ServiceProvider
         $this->bindRecipe($container, ExceptionRenderer::class, ExceptionRenderer::class, [
             new ServiceReference(AuthExceptionMapper::class),
         ]);
+        $this->bindFactory(
+            $container,
+            MaintenanceManager::class,
+            fn() => new MaintenanceManager($app),
+            LifetimeEnum::Singleton,
+        );
 
         $this->bindFactory($container, ErrorHandler::class, fn() => new ErrorHandler(
             logger: static fn(): LoggerInterface => $app->make(HttpExceptionLogger::class),
@@ -54,6 +61,7 @@ final class HttpServiceProvider extends ServiceProvider
         $this->bindRecipe($container, HttpKernel::class, HttpKernel::class, [
             new ServiceReference(RouterKernel::class),
             new ServiceReference(ExecutionScope::class),
+            new ServiceReference(MaintenanceManager::class),
         ]);
 
         $this->bindFactory($container, 'foundation.http', fn() => $container->get(HttpKernel::class), LifetimeEnum::Singleton);
