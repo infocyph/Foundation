bd8743928a024166185dfe02189da2a096b0de9b
A. B. M. Mahmudul Hasan <abmmhasan@users.noreply.github.com>
2026-08-26 13:25:03 +0600

    feat(auth): add oauth consent store contract

---
+++ src/Auth/OAuth/Contract/OAuthConsentStoreInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Infocyph\Foundation\Auth\OAuth\Contract;
+
+use Infocyph\Foundation\Auth\OAuth\Consent\OAuthConsent;
+
+interface OAuthConsentStoreInterface
+{
+    public function findActive(string $accountId, string $clientId, string $scopeFingerprint): ?OAuthConsent;
+
+    public function revoke(string $accountId, string $clientId, int $revokedAt): int;
+
+    public function save(OAuthConsent $consent): void;
+}
