3092122491b2d1ea3aac8320391bd9118ba2bfcc
A. B. M. Mahmudul Hasan <abmmhasan@users.noreply.github.com>
2026-08-26 13:06:31 +0600

    feat(auth): add OAuth access token service contract

---
+++ src/Auth/OAuth/Contract/OAuthAccessTokenServiceInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Infocyph\Foundation\Auth\OAuth\Contract;
+
+use Infocyph\Foundation\Auth\OAuth\Token\OAuthAccessTokenClaims;
+
+interface OAuthAccessTokenServiceInterface
+{
+    public function issue(OAuthAccessTokenClaims $claims): string;
+
+    public function verify(string $token): OAuthAccessTokenClaims;
+}
