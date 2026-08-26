c1ac5a64afcabff3b032976484da48b483cbfe29
A. B. M. Mahmudul Hasan <abmmhasan@users.noreply.github.com>
2026-08-26 13:28:09 +0600

    feat(auth): add safe oauth protocol errors

---
+++ src/Auth/OAuth/Exception/OAuthProtocolException.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Infocyph\Foundation\Auth\OAuth\Exception;
+
+final class OAuthProtocolException extends \RuntimeException
+{
+    public function __construct(
+        public readonly string $error,
+        public readonly string $description,
+        public readonly int $status = 400,
+        public readonly bool $redirectAllowed = false,
+    ) {
+        parent::__construct($error);
+    }
+
+    public static function invalidClient(): self
+    {
+        return new self('invalid_client', 'Client authentication failed.', 401);
+    }
+
+    public static function invalidGrant(): self
+    {
+        return new self('invalid_grant', 'The supplied grant is invalid or no longer usable.');
+    }
+
+    public static function invalidRequest(string $description = 'The OAuth request is invalid.', bool $redirectAllowed = false): self
+    {
+        return new self('invalid_request', $description, 400, $redirectAllowed);
+    }
+
+    public static function invalidScope(bool $redirectAllowed = false): self
+    {
+        return new self('invalid_scope', 'The requested scope is invalid.', 400, $redirectAllowed);
+    }
+
+    public static function unauthorizedClient(bool $redirectAllowed = false): self
+    {
+        return new self('unauthorized_client', 'The client is not allowed to use this request.', 400, $redirectAllowed);
+    }
+
+    public static function unsupportedGrantType(): self
+    {
+        return new self('unsupported_grant_type', 'The requested grant type is not supported.');
+    }
+
+    public static function unsupportedResponseType(bool $redirectAllowed = false): self
+    {
+        return new self('unsupported_response_type', 'The requested response type is not supported.', 400, $redirectAllowed);
+    }
+}
