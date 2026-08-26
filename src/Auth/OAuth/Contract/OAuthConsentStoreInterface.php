c5a1417941e5f3dbde8e6da0d42fdfc525c158eb
A. B. M. Mahmudul Hasan <abmmhasan@users.noreply.github.com>
2026-08-26 15:01:38 +0600

    Support OAuth consent reactivation

--- src/Auth/OAuth/Contract/OAuthConsentStoreInterface.php
+++ src/Auth/OAuth/Contract/OAuthConsentStoreInterface.php
@@ -8,6 +8,8 @@ use Infocyph\Foundation\Auth\OAuth\Consent\OAuthConsent;
 
 interface OAuthConsentStoreInterface
 {
+    public function find(string $accountId, string $clientId, string $scopeFingerprint): ?OAuthConsent;
+
     public function findActive(string $accountId, string $clientId, string $scopeFingerprint): ?OAuthConsent;
 
     public function revoke(string $accountId, string $clientId, int $revokedAt): int;
