72646c0fc9ddc9e510f3ffa4223a62fc9cf5705f
A. B. M. Mahmudul Hasan <abmmhasan@users.noreply.github.com>
2026-08-26 13:06:12 +0600

    feat(auth): add OAuth client type

---
+++ src/Auth/OAuth/Value/OAuthClientType.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Infocyph\Foundation\Auth\OAuth\Value;
+
+enum OAuthClientType: string
+{
+    case Confidential = 'confidential';
+    case Public = 'public';
+}
