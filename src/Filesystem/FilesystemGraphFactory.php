d4e5fbdaf891e4302b27ef5f949ded3ef279f5f6
A. B. M. Mahmudul Hasan <abmmhasan@users.noreply.github.com>
2026-09-02 23:05:08 +0600

    refactor: add filesystem graph factories

---
+++ src/Filesystem/FilesystemGraphFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Infocyph\Foundation\Filesystem;
+
+use Infocyph\Pathwise\StreamHandler\DownloadProcessor;
+use Infocyph\Pathwise\StreamHandler\UploadProcessor;
+use League\Flysystem\FilesystemOperator;
+
+final class FilesystemGraphFactory
+{
+    public static function operator(StorageRegistry $storage): FilesystemOperator
+    {
+        return $storage->disk();
+    }
+
+    public static function upload(FilesystemTransferFactory $transfers): UploadProcessor
+    {
+        return $transfers->upload();
+    }
+
+    public static function download(FilesystemTransferFactory $transfers): DownloadProcessor
+    {
+        return $transfers->download();
+    }
+}
