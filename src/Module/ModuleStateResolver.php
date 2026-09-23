c6811b4e870fa09f005ee4b8454ebdce04993eef
A. B. M. Mahmudul Hasan <abmmhasan@users.noreply.github.com>
2026-09-23 21:14:38 +0600

    style: align empty constructors with PHPForge

--- src/Module/ModuleStateResolver.php
+++ src/Module/ModuleStateResolver.php
@@ -104,7 +104,8 @@ final readonly class ModuleStateResolver
         private Application $application,
         private ModuleCatalog $catalog,
         private ModulePackageStateResolver $packageStates = new ModulePackageStateResolver(),
-    ) {}
+    )
+    {}
 
     /**
      * @phpstan-return list<ModuleState>
