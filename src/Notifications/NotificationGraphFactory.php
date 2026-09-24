284dcdb3b39dfffee26017fff964a7bd1337f7d5
A. B. M. Mahmudul Hasan <abmmhasan@users.noreply.github.com>
2026-09-24 21:33:13 +0600

    remove superseded notification factory workaround

--- src/Notifications/NotificationGraphFactory.php
+++ src/Notifications/NotificationGraphFactory.php
@@ -8,9 +8,6 @@ use Infocyph\Foundation\Config\ConfigRepository;
 use Infocyph\Foundation\Support\ValueNormalizer;
 use Infocyph\TalkingBytes\Email\Config\EmailLimits;
 use Infocyph\TalkingBytes\Email\Emailer;
-use Infocyph\TalkingBytes\Email\EmailMailboxFactory;
-use Infocyph\TalkingBytes\Email\EmailReceiverFactory;
-use Infocyph\TalkingBytes\Email\EmailSenderFactory;
 use Infocyph\TalkingBytes\Email\Parser\RawEmailParser;
 use Infocyph\TalkingBytes\Email\Receiver\SpoolEmailReceiver;
 
@@ -28,21 +25,6 @@ final class NotificationGraphFactory
         );
 
         return EmailLimits::fromArray($limits);
-    }
-
-    public static function emailMailboxFactory(): EmailMailboxFactory
-    {
-        return new EmailMailboxFactory();
-    }
-
-    public static function emailReceiverFactory(): EmailReceiverFactory
-    {
-        return new EmailReceiverFactory();
-    }
-
-    public static function emailSenderFactory(): EmailSenderFactory
-    {
-        return new EmailSenderFactory();
     }
 
     public static function rawEmailParser(EmailLimits $limits): RawEmailParser
