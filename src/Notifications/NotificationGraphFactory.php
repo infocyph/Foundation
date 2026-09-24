17efe6de1eaf272b118b1e72917bd968fa129f89
A. B. M. Mahmudul Hasan <abmmhasan@users.noreply.github.com>
2026-09-24 20:42:10 +0600

    add static-safe TalkingBytes notification factories

--- src/Notifications/NotificationGraphFactory.php
+++ src/Notifications/NotificationGraphFactory.php
@@ -8,11 +8,29 @@ use Infocyph\Foundation\Config\ConfigRepository;
 use Infocyph\Foundation\Support\ValueNormalizer;
 use Infocyph\TalkingBytes\Email\Config\EmailLimits;
 use Infocyph\TalkingBytes\Email\Emailer;
+use Infocyph\TalkingBytes\Email\EmailMailboxFactory;
+use Infocyph\TalkingBytes\Email\EmailReceiverFactory;
+use Infocyph\TalkingBytes\Email\EmailSenderFactory;
 use Infocyph\TalkingBytes\Email\Parser\RawEmailParser;
 use Infocyph\TalkingBytes\Email\Receiver\SpoolEmailReceiver;
 
 final class NotificationGraphFactory
 {
+    public static function emailMailboxFactory(): EmailMailboxFactory
+    {
+        return new EmailMailboxFactory();
+    }
+
+    public static function emailReceiverFactory(): EmailReceiverFactory
+    {
+        return new EmailReceiverFactory();
+    }
+
+    public static function emailSenderFactory(): EmailSenderFactory
+    {
+        return new EmailSenderFactory();
+    }
+
     public static function emailer(EmailProfiles $profiles): Emailer
     {
         return $profiles->sender();
