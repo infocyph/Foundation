3d9d4cab8427dc1733c7c6f1352d1a495e274127
A. B. M. Mahmudul Hasan <abmmhasan@users.noreply.github.com>
2026-09-25 11:12:35 +0600

    refactor(cache): deprecate duplicate Webrick counter wrapper

--- src/Cache/WebrickAtomicCounter.php
+++ src/Cache/WebrickAtomicCounter.php
@@ -5,14 +5,23 @@ declare(strict_types=1);
 namespace Infocyph\Foundation\Cache;
 
 use Infocyph\CacheLayer\Counter\AtomicCounterStoreInterface;
+use Infocyph\Webrick\Interop\CacheLayer\AtomicCounterAdapter;
 use Infocyph\Webrick\Middleware\Throttle\AtomicCounterInterface;
 
+/**
+ * @deprecated Use Webrick's native CacheLayer AtomicCounterAdapter.
+ */
 final readonly class WebrickAtomicCounter implements AtomicCounterInterface
 {
-    public function __construct(private AtomicCounterStoreInterface $store) {}
+    private AtomicCounterAdapter $adapter;
+
+    public function __construct(AtomicCounterStoreInterface $store)
+    {
+        $this->adapter = new AtomicCounterAdapter($store);
+    }
 
     public function increment(string $key, int $delta, int $ttlSeconds): int
     {
-        return $this->store->increment($key, $delta, $ttlSeconds)->value;
+        return $this->adapter->increment($key, $delta, $ttlSeconds);
     }
 }
